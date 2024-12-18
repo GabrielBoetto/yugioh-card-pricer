5d29154893cd4ff19c7b7ef86d596ee195e1e289
Gabriel <gabrielboetto@hotmail.com>
2024-12-18 19:21:50 -0300

    s

---
+++ src/services/cardServices.js
@@ -0,0 +1,15 @@
+// src/services/cardService.js
+import Papa from 'papaparse';
+
+// Función para cargar el archivo CSV
+export const cargarCartas = async () => {
+  const archivoCSV = './data/Yugioh.csv';  // Ruta al archivo CSV (debe ser accesible desde el servidor)
+
+  // Utilizamos fetch para obtener el archivo
+  const response = await fetch(archivoCSV);
+  const textoCSV = await response.text();
+
+  // Usamos PapaParse para procesar el CSV y devolver los datos
+  const resultados = Papa.parse(textoCSV, { header: true });
+  return resultados.data;  // Devuelve los datos como un array de objetos
+};
