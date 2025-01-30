138be8dd04ba3249558c3d8d61401df3d5fd32a4
Gabriel <gabrielboetto@hotmail.com>
2025-01-30 16:15:05 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -70,16 +70,13 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
         <h1>Yu-Gi-Oh! Card Pricer</h1>
         <p className="credits">Desarrollado por Gabriel Boetto.</p>
 =======
-=======
->>>>>>> Stashed changes
         <h1>Yu-Gi-Oh! Card Pricer2</h1>
         <p className="credits">Desarrollado por Gabriel Boetto</p>
->>>>>>> Stashed changes
+
       </header>
 
       <div className="search-container">
